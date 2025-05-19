5b8f190abe202c1acefe86012120f8c685009038
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-05-19 05:37:04 -0300

    Trabajando en cors

--- back/src/server.ts
+++ back/src/server.ts
@@ -6,10 +6,18 @@ import morgan from "morgan";
 
 const server = express();
 
+const allowedOrigins = [
+  "https://shift-application-1iwg.vercel.app", // Reemplaza con tu dominio en Vercel
+  "http://localhost:5173", // Para desarrollo local
+];
 // Configuramos CORS para permitir solicitudes desde otros orígenes.
 // Con la configuración por defecto, se permiten todas las solicitudes, lo cual es adecuado en desarrollo.
 // Si necesitás restringir los orígenes, podés pasar opciones (por ejemplo: { origin: "http://localhost:5173" }).
-server.use(cors());
+server.use(cors({
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 
 // Middleware para registrar las solicitudes en consola (útil en desarrollo)
 server.use(morgan("dev"));
