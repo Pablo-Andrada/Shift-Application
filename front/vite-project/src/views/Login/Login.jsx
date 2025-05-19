46557680f76ff0eb5ee1d617e9d9d2f484c47761
Pablo-Andrada <76768919+Pablo-Andrada@users.noreply.github.com>
2025-05-19 05:47:11 -0300

    working

--- front/vite-project/src/views/Login/Login.jsx
+++ front/vite-project/src/views/Login/Login.jsx
@@ -55,7 +55,7 @@ const Login = ({ onClose }) => {
     
     try {
       console.log("Enviando solicitud de login al backend...");
-      const response = await fetch("http://localhost:3000/user/login", {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/user/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password })
